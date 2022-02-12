c4b70f840741382452a5aab6fe6db5dd98897cb6
Ksenia Prozorova <niakam41@yandex.ru>
2022-02-12 22:06:57 +1200

    review 0.1

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6205f83af8cb1b1d72cb0154',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(PORT);
