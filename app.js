12207382595e7872f79c2351228534a27aefbf70
Ksenia Prozorova <niakam41@yandex.ru>
2022-02-14 02:31:37 +1200

    review 1

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use((req, res) => {
+  res.status(404).send({ message: 'Роут не найден' });
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.listen(PORT);
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { ERROR_CODE, UNDEFINED_CODE } = require('../utils/errors');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
@@ -7,53 +8,73 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUsersId = (req, res) => {
-  User.find({})
+  User.findById(req.params.userId)
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(UNDEFINED_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+      }
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE).send({ message: 'Передан невалидный id' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  const ERROR_CODE = 400;
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => {
-      res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  const ERROR_CODE = 400;
-  const UNDEFINED_CODE = 404;
 
   User.findByIdAndUpdate(req.user._id, { name, about }, {
     new: true,
     runValidators: true,
   })
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch(() => {
-      res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
-      res.status(UNDEFINED_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(UNDEFINED_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+      }
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении профиля' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  const ERROR_CODE = 400;
-  const UNDEFINED_CODE = 404;
 
   User.findByIdAndUpdate(req.user._id, { avatar }, {
     new: true,
     runValidators: true,
   })
+    .orFail()
     .then((user) => res.send({ data: user }))
-    .catch(() => {
-      res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
-      res.status(UNDEFINED_CODE).send({ message: 'Пользователь с указанным _id не найден' });
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(UNDEFINED_CODE).send({ message: 'Пользователь с указанным _id не найден' });
+      }
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при обновлении аватара' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка' });
+      }
     });
 };
